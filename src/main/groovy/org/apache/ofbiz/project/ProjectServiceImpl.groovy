916b08cb4eb56ea8672ed79bcea5d40771a1ec12
Leila <leila.mekika@nereide.fr>
2021-09-08 16:46:27 +0200

    TEST /!\ de refacto

--- src/main/groovy/org/apache/ofbiz/project/ProjectServiceImpl.groovy
+++ src/main/groovy/org/apache/ofbiz/project/ProjectServiceImpl.groovy
@@ -2,6 +2,7 @@ package org.apache.ofbiz.project
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.xml.DomElement
 import com.intellij.util.xml.DomFileElement
 import com.intellij.util.xml.DomService
 import org.apache.ofbiz.dom.ControllerFile
@@ -9,6 +10,9 @@ import org.apache.ofbiz.dom.ControllerFile.RequestMap
 import org.apache.ofbiz.dom.EntityModelFile
 import org.apache.ofbiz.dom.EntityModelFile.Entity
 import org.apache.ofbiz.dom.EntityModelFile.ViewEntity
+
+import java.util.function.BiFunction
+import java.util.function.Function
 
 class ProjectServiceImpl implements ProjectServiceInterface {
     private final Project project
@@ -18,45 +22,34 @@ class ProjectServiceImpl implements ProjectServiceInterface {
     }
 
     RequestMap getControllerUri(String name) {
-        List<DomFileElement<ControllerFile>> controllerFiles = this.getControllerFiles()
-        for (DomFileElement<ControllerFile> controllerFile : controllerFiles) {
-            List<RequestMap> requestMaps = controllerFile.getRootElement().getRequestMap()
-            for (RequestMap request : requestMaps) {
-                if (request.getUri().getValue().equalsIgnoreCase(name)) return request
-            }
-        }
-        return null
+        return getMatchinTagFromClassFile(ControllerFile.class, ControllerFile.&getRequestMap, RequestMap.&getUri, name);
     }
 
     Entity getEntity(String name){
-        List<DomFileElement<EntityModelFile>> entityFiles = getEntityModelFiles()
-        for (DomFileElement<EntityModelFile> entityFile : entityFiles) {
-            List<Entity> entities = entityFile.getRootElement().getEntities()
-            for (Entity entity : entities) {
-                if (entity.getEntityName().getValue().equalsIgnoreCase(name)) return entity
-            }
-        }
-        return null
+        return getMatchinTagFromClassFile(EntityModelFile.class, EntityModelFile.&getEntities, Entity.&getEntityName, name);
     }
 
     ViewEntity getViewEntity(String name){
-        List<DomFileElement<EntityModelFile>> entityFiles = getEntityModelFiles()
-        for (DomFileElement<EntityModelFile> entityFile : entityFiles) {
-            List<ViewEntity> entities = entityFile.getRootElement().getViewEntities()
-            for (ViewEntity entity : entities) {
-                if (entity.getEntityName().getValue().equalsIgnoreCase(name)) return entity
+        return getMatchinTagFromClassFile(EntityModelFile.class, EntityModelFile.&getViewEntities, ViewEntity.&getEntityName, name);
+    }
+
+
+    private List<DomFileElement<?>> getMatchinTagFromClassFile(Class classFile,
+                                                               Function getFileElementMethod,
+                                                               Function getElementValueMethod,
+                                                               String matchingValue ) {
+        List<DomFileElement<?>> projectFiles = getClassProjectMatchingFiles(classFile, this.project)
+        for (DomFileElement<?> projectFile : projectFiles) {
+            List<DomElement> elements = getFileElementMethod.apply(projectFile.getRootElement())
+            for (DomElement element : elements) {
+                if (getElementValueMethod.apply(element).getValue().equalsIgnoreCase(matchingValue)) return element
             }
         }
         return null
     }
 
-    private List<DomFileElement<ControllerFile>> getControllerFiles() {
-        return DomService.getInstance().getFileElements(ControllerFile.class, this.project, GlobalSearchScope.allScope(this.project))
+    private static List<DomFileElement<?>> getClassProjectMatchingFiles (Class classFile, Project project) {
+        return DomService.getInstance().getFileElements( classFile , project, GlobalSearchScope.allScope(project));
     }
-
-    private List<DomFileElement<EntityModelFile>> getEntityModelFiles() {
-        return DomService.getInstance().getFileElements(EntityModelFile.class, this.project, GlobalSearchScope.allScope(this.project))
-    }
-
     // TODO : Générifier la récupération d'un type particulier de fichier ?
 }
