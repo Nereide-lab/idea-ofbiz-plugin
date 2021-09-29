35cdf50215a6368afc72cbeea312a63725c2e44a
cgaetan <gaetan.chaboussie@nereide.fr>
2021-09-29 17:52:49 +0200

    Application aux menus de la structure optimisée

--- src/main/groovy/org/apache/ofbiz/reference/xml/MenuReference.groovy
+++ src/main/groovy/org/apache/ofbiz/reference/xml/MenuReference.groovy
@@ -1,25 +1,24 @@
 package org.apache.ofbiz.reference.xml
 
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiReferenceBase
 import com.intellij.psi.xml.XmlAttributeValue
-import com.intellij.psi.xml.XmlElement
-import com.intellij.util.xml.DomElement
-import org.apache.ofbiz.project.ProjectServiceInterface
+import org.apache.ofbiz.dom.MenuFile
 import org.jetbrains.annotations.Nullable
 
-import java.util.regex.Matcher
-import java.util.regex.Pattern
+class MenuReference extends GenericXmlReference {
 
-class MenuReference extends PsiReferenceBase<XmlElement> {
+
     MenuReference(XmlAttributeValue menuName, boolean soft) {
-        super(menuName, soft)
+        super(menuName, soft,
+                "getMenus",
+                "getName",
+                "getMenu",
+                MenuFile.class)
     }
 
     @Nullable
     PsiElement resolve() {
-        ProjectServiceInterface structureService = this.getElement().getProject().getService(ProjectServiceInterface.class)
-        DomElement definition = structureService.getMenu(this.getValue())
-        return definition != null ? definition.getXmlElement() : null
+        super.resolve()
     }
+
 }
