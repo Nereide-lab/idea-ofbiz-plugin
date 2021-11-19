86f64c1bb0d7707fdf020fdc98c1885fe6ebec3d
cgaetan <gaetan.chaboussie@nereide.fr>
2021-11-19 16:41:38 +0100

    Added two passing tests

--- src/test/resources/testData/EntityReference/EntityReferenceWithFindMethodFromDelegator/MyTestClass1.java
+++
@@ -1,9 +0,0 @@
-package org.apache.ofbiz;
-
-import org.apache.ofbiz.entity.Delegator;
-
-public class MyTestClass1 {
-    public void myTestFunction(){
-        var testData = Delegator.find("MyTest<caret>Entity");
-    }
-}
