93df8fcd8d986831d261ab9d680e8a9ad7f27889
cgaetan <gaetan.chaboussie@nereide.fr>
2021-12-17 15:47:39 +0100

    Correction d'un bug de référence vers les écrans depuis un fichier controller

--- src/main/groovy/fr/nereide/reference/xml/GenericXmlReference.groovy
+++ src/main/groovy/fr/nereide/reference/xml/GenericXmlReference.groovy
@@ -27,6 +27,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.psi.xml.XmlTag
 import com.intellij.util.xml.DomElement
 import com.intellij.util.xml.DomManager
+import fr.nereide.dom.ScreenFile
 import fr.nereide.project.ProjectServiceInterface
 import fr.nereide.project.utils.FileHandlingUtils
 
@@ -35,7 +36,7 @@ import java.util.regex.Pattern
 
 class GenericXmlReference extends PsiReferenceBase<XmlAttributeValue> {
     private static final Logger LOG = Logger.getInstance(GenericXmlReference.class)
-    String elementName
+    //String elementName
     final String ELEMENT_LIST_GETTER_METHOD
     final String ELEMENT_NAME_GETTER_METHOD
     final String ELEMENT_GETTER_METHOD
@@ -50,41 +51,42 @@ class GenericXmlReference extends PsiReferenceBase<XmlAttributeValue> {
         this.ELEMENT_LIST_GETTER_METHOD = ELEMENT_LIST_GETTER_METHOD
         this.ELEMENT_GETTER_METHOD = ELEMENT_GETTER_METHOD
         this.FILE_TYPE = FILE_TYPE
-        this.elementName = setElementName(element)
+        //this.getElement.getName() = setElementName(element)
     }
 
     @Override
     PsiElement resolve() {
         ProjectServiceInterface structureService = this.getElement().getProject().getService(ProjectServiceInterface.class)
         DomManager dm = DomManager.getDomManager(this.getElement().getProject())
-
-        XmlTag parentTag = (XmlTag) getTag(element)
-        if (parentTag) {
-            PsiElement locationAttribute = parentTag.getAttribute('location')
+        XmlTag containingTag = (XmlTag) getTag(this.getElement())
+        if (containingTag) {
+            PsiElement locationAttribute = containingTag.getAttribute('location')
             if (locationAttribute) {
                 //On va chercher dans le fichier ciblé
                 String locationAttributeValue = locationAttribute.getValue()
                 PsiFile targetedFile = FileHandlingUtils.getTargetFile(locationAttributeValue, structureService)
-                return FileHandlingUtils.getElementFromSpecificFile(targetedFile, dm, this.elementName, this.FILE_TYPE,
+                return FileHandlingUtils.getElementFromSpecificFile(targetedFile, dm, this.getElement().getValue(), this.FILE_TYPE,
                         this.ELEMENT_NAME_GETTER_METHOD, this.ELEMENT_LIST_GETTER_METHOD)
+            } else if (isPageReferenceFromController(containingTag)) {
+                return resolveScreenInController(this.getElement(), structureService, dm)
             } else if (isInRightFile(this.getElement(), this.FILE_TYPE, dm)) {
                 // On regarde dans le fichier courant
                 PsiFile currentFile = this.getElement().getContainingFile()
-                return FileHandlingUtils.getElementFromSpecificFile(currentFile, dm, this.elementName, this.FILE_TYPE,
+                return FileHandlingUtils.getElementFromSpecificFile(currentFile, dm, this.getElement().getValue(), this.FILE_TYPE,
                         this.ELEMENT_NAME_GETTER_METHOD, this.ELEMENT_LIST_GETTER_METHOD)
             } else {
                 // Si on a pas de location, mais qu'on est pas dans un fichier pertinent => standard
-                return resolveStandard(structureService)
+                return resolveStandard(structureService, this.getElement())
             }
         } else {
             // Si pas de tag xml trouvé ou problème, on utilise la methode normale
-            return resolveStandard(structureService)
+            return resolveStandard(structureService, this.getElement())
         }
     }
 
-    private XmlElement resolveStandard(ProjectServiceInterface structureService) {
+    private XmlElement resolveStandard(ProjectServiceInterface structureService, def element) {
         String elementGetterMethod = this.ELEMENT_GETTER_METHOD
-        DomElement definition = structureService."$elementGetterMethod"(elementName)
+        DomElement definition = structureService."$elementGetterMethod"(element.getValue())
         return definition != null ? definition.getXmlElement() : null
     }
 
@@ -114,15 +116,28 @@ class GenericXmlReference extends PsiReferenceBase<XmlAttributeValue> {
         return dm.getFileElement(currentFile, FILE_TYPE) != null
     }
 
+    static boolean isPageReferenceFromController(XmlTag containingTag) {
+        return (containingTag.getAttribute('page') != null)
+    }
+
+    static PsiElement resolveScreenInController(XmlAttributeValue element, ProjectServiceInterface structureService, DomManager dm) {
+        String screenName = getScreenName(element)
+        String stringValue = element.getValue()
+        String fileComponentLocation = stringValue.substring(0, stringValue.length() - screenName.length() - 1)
+        PsiFile targetedFile = FileHandlingUtils.getTargetFile(fileComponentLocation, structureService)
+        return FileHandlingUtils.getElementFromSpecificFile(targetedFile, dm, screenName, ScreenFile.class,
+                "getName", "getScreens")
+    }
+
     /**
      * For the case of screen name in controller
      * @param name
      * @return
      */
-    private static String setElementName(XmlAttributeValue name) {
+    private static String getScreenName(XmlAttributeValue name) {
         //regex that gets the screen name
         final Pattern SCREEN_NAME_PATTERN = Pattern.compile("[^#]*\$")
         Matcher matcher = SCREEN_NAME_PATTERN.matcher(name.getValue())
-        return matcher.find() ? matcher.group(0) : name.getValue()
+        return matcher.find() ? matcher.group(0) : null
     }
 }
