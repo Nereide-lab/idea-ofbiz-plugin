dae976be36b0ef7c103b6c92cbd81da41202751f
cgaetan <gaetan.chaboussie@nereide.fr>
2021-12-21 09:57:00 +0100

    Refacto for package clarity, base scructure for completion, and moved patterns to general class

---
+++ src/main/groovy/fr/nereide/reference/contributor/GroovyReferenceContributor.groovy
@@ -0,0 +1,66 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License") you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package fr.nereide.reference.contributor
+
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceProvider
+import com.intellij.psi.PsiReferenceRegistrar
+import com.intellij.util.ProcessingContext
+import fr.nereide.project.OfbizPatterns
+import fr.nereide.reference.groovy.EntityGroovyReference
+import fr.nereide.reference.groovy.ServiceGroovyReference
+import fr.nereide.reference.groovy.UiLabelGroovyReference
+import org.jetbrains.annotations.NotNull
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.literals.GrLiteral
+
+class GroovyReferenceContributor extends PsiReferenceContributor {
+    GroovyReferenceContributor() {}
+
+    void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
+        registrar.registerReferenceProvider(OfbizPatterns.GROOVY_SERVICE_CALL, new PsiReferenceProvider() {
+            @NotNull
+            PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                GrLiteral el = (GrLiteral) element
+                ServiceGroovyReference service = new ServiceGroovyReference(el, true)
+                PsiReference[] reference = (PsiReference) service
+                return reference
+            }
+        })
+        registrar.registerReferenceProvider(OfbizPatterns.GROOVY_ENTITY_CALL, new PsiReferenceProvider() {
+            @NotNull
+            PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                GrLiteral el = (GrLiteral) element
+                EntityGroovyReference entity = new EntityGroovyReference(el, true)
+                PsiReference[] reference = (PsiReference) entity
+                return reference
+            }
+        })
+        registrar.registerReferenceProvider(OfbizPatterns.GROOVY_LABEL_CALL, new PsiReferenceProvider() {
+            @NotNull
+            PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                GrLiteral el = (GrLiteral) element
+                UiLabelGroovyReference label = new UiLabelGroovyReference(el, true)
+                PsiReference[] reference = (PsiReference) label
+                return reference
+            }
+        })
+    }
+}
---
+++ src/main/groovy/fr/nereide/reference/contributor/JavaReferenceContributor.groovy
@@ -0,0 +1,66 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License") you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package fr.nereide.reference.contributor
+
+
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiLiteralExpression
+import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceContributor
+import com.intellij.psi.PsiReferenceProvider
+import com.intellij.psi.PsiReferenceRegistrar
+import com.intellij.util.ProcessingContext
+import fr.nereide.project.OfbizPatterns
+import fr.nereide.reference.java.EntityJavaReference
+import fr.nereide.reference.java.ServiceJavaReference
+import fr.nereide.reference.java.UiLabelJavaReference
+import org.jetbrains.annotations.NotNull
+
+class JavaReferenceContributor extends PsiReferenceContributor {
+    JavaReferenceContributor() {}
+
+    void registerReferenceProviders(PsiReferenceRegistrar registrar) {
+        registrar.registerReferenceProvider(OfbizPatterns.JAVA_SERVICE_CALL, new PsiReferenceProvider() {
+            @NotNull
+            PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                PsiLiteralExpression el = (PsiLiteralExpression) element
+                ServiceJavaReference service = new ServiceJavaReference(el, true)
+                PsiReference[] reference = (PsiReference) service
+                return reference
+            }
+        })
+        registrar.registerReferenceProvider(OfbizPatterns.JAVA_ENTITY_CALL, new PsiReferenceProvider() {
+            @NotNull
+            PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                PsiLiteralExpression el = (PsiLiteralExpression) element
+                EntityJavaReference entity = new EntityJavaReference(el, true)
+                PsiReference[] reference = (PsiReference) entity
+                return reference
+            }
+        })
+        registrar.registerReferenceProvider(OfbizPatterns.JAVA_LABEL_CALL, new PsiReferenceProvider() {
+            @NotNull
+            PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                PsiLiteralExpression el = (PsiLiteralExpression) element
+                UiLabelJavaReference label = new UiLabelJavaReference(el, true)
+                PsiReference[] reference = (PsiReference) label
+                return reference
+            }
+        })
+    }
+}
