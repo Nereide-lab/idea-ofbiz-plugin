5a836fb2a9674ae70842acd5cb57997ca35a2b97
cgaetan <gaetan.chaboussie@nereide.fr>
2022-02-16 10:35:36 +0100

    Ajout d'une première structure de récupération du nom de l'entité dans la déclaration de la GV

--- src/main/groovy/fr/nereide/completion/provider/common/EntityFieldNameCompletionProvider.groovy
+++ src/main/groovy/fr/nereide/completion/provider/common/EntityFieldNameCompletionProvider.groovy
@@ -23,15 +23,41 @@ import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.psi.PsiElement
 import com.intellij.util.ProcessingContext
 import org.jetbrains.annotations.NotNull
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.GrVariable
+import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrReferenceExpression
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class EntityFieldNameCompletionProvider extends CompletionProvider<CompletionParameters> {
+    private static final Logger LOG = Logger.getInstance(EntityFieldNameCompletionProvider.class)
+
 
     @Override
     protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
         PsiElement element = parameters.getPosition()
+        try {
+            PsiElement genericValueRef = element.getParent().getFirstChild()
+            assert genericValueRef instanceof GrReferenceExpression
+
+            PsiElement initialVariable = genericValueRef.resolve()
+            assert initialVariable instanceof GrVariable
+
+            String declarationExpr = initialVariable.getInitializerGroovy().getText()
+
+            final Pattern ENTITY_NAME_PATTERN = Pattern.compile("(['\"](.*?)['\"])")
+            Matcher matcher = ENTITY_NAME_PATTERN.matcher(declarationExpr)
+
+            String entityName = matcher.find() ? matcher.group(0) : null
+
+            String str = 'trolo'
+        } catch (Exception e) {
+            LOG.info(e)
+        }
         LookupElement el = LookupElementBuilder.create('GENERIC_VALUE_ATTRIBUTE_TROLO')
         result.addElement(PrioritizedLookupElement.withPriority(el, 3000))
     }
