99149b03c3ef2761c5294f1c24b8f929f094901d
cgaetan <gaetan.chaboussie@nereide.fr>
2022-06-03 16:40:33 +0200

    REFACTO DE L'ESPAAAAACE ! Refacto de l'arbo des tests et notament des fichers de tests, afin de coller à l'architecture plus lisible des tests de complétion. Nettoyage au passage de fichiers et sous dossiers inutiles et encombrants.

--- src/test/resources/testData/JavaEntityReference/EntityReferenceWithFindOneMethod/MyTestClass.java
+++
@@ -1,9 +0,0 @@
-package org.apache.ofbiz;
-
-import org.apache.ofbiz.entity.Delegator;
-
-public class MyTestClass {
-    public void myTestFunction() {
-        var testData = Delegator.findOne("Piltov<caret>erData", null, null);
-    }
-}
