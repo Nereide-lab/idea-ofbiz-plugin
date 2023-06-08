49be3089eb8401a57e5da00643bff647406e8ab6
cgaetan <gaetan.chaboussie@nereide.fr>
2023-06-08 16:46:44 +0200

    Updated licence

--- src/test/resources/testData/completion/groovy/ServiceCompletionInGroovyFile.groovy
+++ src/test/resources/testData/completion/groovy/ServiceCompletionInGroovyFile.groovy
@@ -1,13 +1,14 @@
-package org.apache.ofbiz
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
- * "License") you may not use this file except in compliance
+ * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -15,6 +16,9 @@ package org.apache.ofbiz
  * specific language governing permissions and limitations
  * under the License.
  */
+
+package org.apache.ofbiz
+
 
 import org.apache.ofbiz.service.LocalDispatcher
 
