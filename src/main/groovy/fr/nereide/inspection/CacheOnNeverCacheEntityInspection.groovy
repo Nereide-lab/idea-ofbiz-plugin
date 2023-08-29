5f8e5c3cd9272c8c105e501daeac6ec85d5b93e3
cgaetan <gaetan.chaboussie@nereide.fr>
2023-08-29 17:57:14 +0200

    Changed to cleaner version of wronc use of cache method detection

--- src/main/groovy/fr/nereide/inspection/CacheOnNeverCacheEntityInspection.groovy
+++ src/main/groovy/fr/nereide/inspection/CacheOnNeverCacheEntityInspection.groovy
@@ -23,14 +23,20 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.JavaElementVisitor
+import com.intellij.psi.JavaPsiFacade
+import com.intellij.psi.PsiClass
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
-import com.intellij.psi.PsiIdentifier
+import com.intellij.psi.PsiMethod
 import com.intellij.psi.PsiMethodCallExpression
+import com.intellij.psi.PsiReferenceExpression
+import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiTreeUtil
 import fr.nereide.completion.provider.common.EntityFieldCompletionProvider
 import fr.nereide.dom.EntityModelFile
 import fr.nereide.inspection.quickfix.RemoveCacheCallFix
 import fr.nereide.project.ProjectServiceInterface
+import fr.nereide.project.pattern.OfbizPatternConst
 import org.jetbrains.annotations.NotNull
 
 class CacheOnNeverCacheEntityInspection extends LocalInspectionTool {
@@ -47,26 +53,41 @@ class CacheOnNeverCacheEntityInspection extends LocalInspectionTool {
     PsiElementVisitor buildVisitor(@NotNull final ProblemsHolder holder, boolean isOnTheFly) {
         return new JavaElementVisitor() {
             @Override
-            void visitIdentifier(@NotNull PsiIdentifier identifier) {
-                // TODO : Surely there is a (cleaner) way to do this with java facade rather than psi text.
-                if (!identifier.text.contains('cache')) return
-                PsiMethodCallExpression query = PsiTreeUtil.getParentOfType(identifier, PsiMethodCallExpression)
+            void visitReferenceExpression(PsiReferenceExpression exp) {
+                PsiElement method = exp.resolve()
+                if (!method || !method instanceof PsiMethod) return
+                method = method as PsiMethod
+
+                if (!isCacheFromEntityQuery(method)) return
+                PsiMethodCallExpression query = PsiTreeUtil.getParentOfType(exp, PsiMethodCallExpression)
                 String entityName = EntityFieldCompletionProvider.getEntityNameFromDeclarationString(query.text)
                 if (!entityName) return
-                ProjectServiceInterface service = identifier.getProject().getService(ProjectServiceInterface.class)
+
+                ProjectServiceInterface service = exp.getProject().getService(ProjectServiceInterface.class)
                 EntityModelFile.Entity entity = service.getEntity(entityName)
                 if (!entity) return
-                String neverCache = entity.getNeverCache()?:''
-                if(!neverCache) return
-                if(neverCache && neverCache == 'true') {
-                    holder.registerProblem(
-                            identifier,
-                            InspectionBundle.message('inspection.entity.cache.on.never.cache.display.descriptor'),
-                            ProblemHighlightType.WARNING,
-                            myQuickFix
-                    )
-                }
+
+                String neverCache = entity.getNeverCache() ?: ''
+                if (!neverCache || neverCache != 'true') return
+
+                List<PsiMethodCallExpression> parents = PsiTreeUtil.collectParents(exp, PsiMethodCallExpression.class, false,
+                        el -> { el instanceof PsiMethod })
+                PsiMethodCallExpression fullQuery = parents.find { it.nextSibling.text == ';' }
+                if (!fullQuery) return
+
+                holder.registerProblem(fullQuery,
+                        InspectionBundle.message('inspection.entity.cache.on.never.cache.display.descriptor'),
+                        ProblemHighlightType.WARNING,
+                        myQuickFix
+                )
             }
+
         }
+    }
+
+    static boolean isCacheFromEntityQuery(PsiMethod method) {
+        PsiClass entityQueryClass = JavaPsiFacade.getInstance(method.getProject())
+                .findClass(OfbizPatternConst.ENTITY_QUERY_CLASS, GlobalSearchScope.allScope(method.getProject()))
+        return entityQueryClass.getMethods().contains(method) && method.getName() == 'cache'
     }
 }
