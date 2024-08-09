af3e0da86b8b9f3c5d23efdfd1b31eae11c85811
cgaetan <gaetan.chaboussie@nereide.fr>
2024-08-09 15:44:52 +0200

    [24.2-update] Fix broken java generic value field completion test

--- src/test/resources/testData/completion/java/EntityFieldCompletionInGetFieldFromGVListInForLoop.java
+++ src/test/resources/testData/completion/java/EntityFieldCompletionInGetFieldFromGVListInForLoop.java
@@ -2,7 +2,6 @@ package org.apache.ofbiz;
 
 import java.util.List;
 
-import com.intellij.util.xml.GenericValue;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.entity.util.EntityQuery;
