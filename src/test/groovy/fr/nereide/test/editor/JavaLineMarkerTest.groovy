0231fc1a126d06e3609c9d3c5858482420ac9cca
cgaetan <gaetan.chaboussie@nereide.fr>
2025-06-02 15:27:59 +0200

    [#21] Creates the Test Structure for multi lang.

---
+++ src/test/groovy/fr/nereide/test/editor/JavaLineMarkerTest.groovy
@@ -0,0 +1,18 @@
+package fr.nereide.test.editor
+
+import com.intellij.psi.PsiLiteralExpression
+import fr.nereide.editor.marker.JavaServiceEcaMarkerProvider
+
+class JavaLineMarkerTest extends BaseLineMarkerTest {
+
+    protected String getExtension() { return 'java' }
+
+    @Override
+    protected Class getElementTypeToFind() {
+        return PsiLiteralExpression.class
+    }
+
+    void testServiceEcaMarkerInJava() {
+        doTest(new JavaServiceEcaMarkerProvider(), 'Eca(s) present on service')
+    }
+}
--- src/test/resources/testData/editor/ServiceEcaMarkerInJava.java
+++
@@ -1,9 +0,0 @@
-package org.apache.ofbiz
-import org.apache.ofbiz.service.LocalDispatcher
-
-class ServiceEcaMarkerInJava {
-    void myTestFunction() {
-        LocalDispatcher dispatcher = new LocalDispatcher()
-        var result = dispatcher.runSync("ServiceEcaMarkerInJavaService", null)
-    }
-}
